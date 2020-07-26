ecefb8343a8cb3cec0a9259b3a7406eb9d91be7b
Muhammad Fauzi Masykur <muhammad.masykur@go-jek.com>
2020-07-26 14:21:07 +0700

    init project

---
+++ ios/GoPay-Tokenization-Swift/GoPay-Tokenization-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoPay-Tokenization-Swift
+//
+//  Created by Muhammad Fauzi Masykur on 26/07/20.
+//  Copyright © 2020 Muhammad Fauzi Masykur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
